493c2ad71c74100d5dc8550a117a126cf75474ec
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-09-03 22:51:27 +0200

    Initial commit

---
+++ src/main/java/com/asosiy/idoston/IdostonApplication.java
@@ -0,0 +1,13 @@
+package com.asosiy.idoston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdostonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdostonApplication.class, args);
+	}
+
+}
