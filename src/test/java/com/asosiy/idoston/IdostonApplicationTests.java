db6deeebee46a2da0507b505d0c6e8e31e01a79f
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-09-09 22:42:45 +0200

    Mockito test

--- src/test/java/com/asosiy/idoston/IdostonApplicationTests.java
+++ src/test/java/com/asosiy/idoston/IdostonApplicationTests.java
@@ -1,16 +1,57 @@
 package com.asosiy.idoston;
 
+import com.asosiy.idoston.model.Ticket;
+import com.asosiy.idoston.repository.TicketRepository;
+import com.asosiy.idoston.service.TicketService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class IdostonApplicationTests {
+
+
+//	@Autowired
+//	private TicketRepository ticketRepository;
+
+	@Mock
+	TicketService ticketService;
+
+//	@InjectMocks
+//	TicketService ticketService;
+
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetAllTickets(){
+
+		List<Ticket> ticketList = new ArrayList<>();
+		Ticket ticket = new Ticket(10, "Berlin", "desc", "date", "some url", "a location", "True", 1);
+		ticketList.add(ticket);
+		when(ticketService.getAllTickets()).thenReturn(ticketList);
+
+		assertEquals(1, ticketService.getAllTickets().size());
+
+		assertEquals("Berlin", ticketService.getAllTickets().get(0).getName());
+		assertEquals("date", ticketService.getAllTickets().get(0).getPubdate());
+
 	}
 
 }
